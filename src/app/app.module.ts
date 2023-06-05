f8db2901b4ae9a791ee10f91c7ac03536f760e3b
Abu Suraj <abusurajbd@gmail.com>
2023-06-06 01:14:22 +0600

    add boostrap and add-product form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AddProductComponent } from './add-product/add-product.component';
 import { ProductsDetailComponent } from './products-detail/products-detail.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
