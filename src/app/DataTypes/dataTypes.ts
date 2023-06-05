537d470eb342ff2325ef9abb93d7d257cf68f37d
Abu Suraj <abusurajbd@gmail.com>
2023-06-06 01:33:24 +0600

    add product data types Interface

---
+++ src/app/DataTypes/dataTypes.ts
@@ -0,0 +1,7 @@
+export interface product {
+    id: any;
+    name: string;
+    price: number;
+    image: string;
+    description: string;
+}
--- src/app/services/produtcs.service.ts
+++ src/app/services/produtcs.service.ts
@@ -7,4 +7,6 @@ import { HttpClient } from '@angular/common/http';
 export class ProdutcsService {
 
   constructor(private http: HttpClient) { }
+
+  
 }
