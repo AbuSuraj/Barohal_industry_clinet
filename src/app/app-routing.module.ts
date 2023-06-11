8525e2888d6168ee58d4c9ed02a552c589ff3696
AbuSuraj <abusurajbd@gmail.com>
2023-06-12 02:10:10 +0600

    add product-list table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductsDetailComponent } from './products-detail/products-detail.component';
 import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'add-product', component: AddProductComponent}, 
   { path: 'product-details', component: ProductsDetailComponent}, 
+  { path: 'product-list', component: ProductListComponent}, 
 ];
 
 @NgModule({
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { product } from '../DataTypes/dataTypes';
+import { ProdutcsService } from '../services/produtcs.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  public sL = 1;
+  product_list: product [] = [];
+
+  constructor(private productService: ProdutcsService){}
+
+  ngOnInit(): void {
+      this.products();
+  }
+    
+    products(){
+      this.productService.getProducts().subscribe(result =>{
+        this.product_list = result;
+        console.log(this.product_list)
+      })
+    }
 }
